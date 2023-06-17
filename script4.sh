d355d825964f7eb60ef773968db7531d3acedf35
Shaunak Rananaware <113698409+shaunakrananaware@users.noreply.github.com>
2023-06-17 15:43:07 +0530

    Delete script4.sh

--- script4.sh
+++
@@ -1,5 +0,0 @@
-cpu_idle=`top -b -n 1|grep Cpu |awk '{print $8}'|cut -f 1 -d "."`
-echo "CPU Usage: $((100 - $cpu_idle))%"
-
-mem_free=`free -m | grep "Mem" | awk '{print $4+$6}'`
-echo "Memory available: $mem_free MB"
